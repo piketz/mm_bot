d240fc82c1dc027ea304b812fca981206012c3e0
ub <ub@local>
2026-03-24 15:26:02 +0500

    Remove sensitive files

--- servicedesk.py
+++ servicedesk.py
@@ -15,8 +15,7 @@ from cryptography.fernet import Fernet
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import ContextTypes
 
-# Для совместимости с mm_bot
-import servicedesk
+
 
 # === КОНФИГУРАЦИЯ ===
 BASE_URL = "https://mobilebmc.tander.ru"
